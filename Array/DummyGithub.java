2a331fe846769688727d3e88b6f4a3b6a648c98d
Rohan baranwal <rohan.baranwal@jungleegames.com>
2022-02-10 22:52:32 +0530

    checking for this file

---
+++ Array/DummyGithub.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class DummyGithub {
+    public static void main(String[] args) {
+        method();
+    }
+    public static void method(){
+        System.out.println("my name is Rnit");
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            arr.add(2*i);
+        }
+        System.out.println(arr);
+    }
+}
